f2ff2e79676500e1583bc4d120811f01478b9c1c
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-04-06 16:47:58 +0200

    Update BottomSheet.swift

--- Sources/BottomSheet/BottomSheet.swift
+++ Sources/BottomSheet/BottomSheet.swift
@@ -150,34 +150,3 @@ struct BottomRoundedRectangle: Shape {
         return path
     }
 }
-
-struct BottomSheetShowcase: View {
-    @State var showBottomSheet = true
-    var body: some View {
-        NavigationView {
-            Form {
-                NavigationLink("BottomSheet with all customizations applied") {
-                    showCase1
-                }
-                NavigationLink("Plain BottomSheet") {
-                    showCase2
-                }
-            }
-        }
-    }
-    var showCase1: some View {
-        Text("Tap to Toggle BottomSheet")
-            .onTapGesture {
-                showBottomSheet.toggle()
-            }
-            .bottomSheet(isPresented: $showBottomSheet, interactiveDismiss: true, height: 75, maxHeight: 250) {
-                Text("Hello")
-            }
-    }
-    var showCase2: some View {
-        Text("Plain BottomSheet (only Height Parameter)")
-            .bottomSheet(height: 100) {
-                Text("hello")
-            }
-    }
-}
